464aa19761063cd6a58100ca596b6e50014cff83
chutima puthachon <chutima.p8@gmail.com>
2024-11-04 14:52:11 +1100

    added setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from config import config
 
-def create_app():
+db = SQLAlchemy()
+
+def create_app(config_class=config.DevelopmentConfig):
     """ method used to create an app instance """
     app = Flask(__name__)
+    db.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     # Register the namespaces
@@ -15,5 +20,6 @@ def create_app():
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
 
     return app
