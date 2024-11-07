610ccd6371db21b7a205978941f8e5843e2e9230
Chutima / Giv <137391745+Giv-08@users.noreply.github.com>
2024-11-08 09:50:18 +1100

    Update place.py

--- app/models/place.py
+++ app/models/place.py
@@ -6,16 +6,9 @@ from app.persistence.user_repository import User
 from datetime import datetime
 # from flask_bcrypt import Bcrypt
 from sqlalchemy import Column, String, Float, DateTime, Boolean, ForeignKey, Table
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 
 # bcrypt = Bcrypt()
-
-# place_amenity = Table('place_amenity', Base.metadata,
-#                       Column('place_id', String(60), ForeignKey(
-#                           'places.id'), primary_key=True),
-#                       Column('amenity_id', String(60), ForeignKey(
-#                           'amenities.id'), primary_key=True)
-#                       )
 
 class Place(Base):
     """ Place class """
